5b58dec92e74e43b28abfd853c2e5e44a69f450a
Faisal <m.faisalraufi@gmail.com>
2025-01-17 03:54:58 +0300

    Initial commit for Redmine repository plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope '/projects/:project_id' do
-    resources :repositories, only: [:index, :show], controller: 'repositories' do
+    resources :custom_repositories, only: [:index, :show], controller: 'custom_repositories' do
       member do
         get 'revisions/:rev', action: :revision, as: :revision
         get 'raw/:rev/*path', action: :raw_file, as: :raw_file
--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ Redmine::Plugin.register :redmine_repository_plugin do
   name 'Redmine Repository Plugin'
   author 'MR Faisal'
   description 'A plugin to add repository functionality to projects'
-  version '0.1.0'
-  url 'it is in somewhere in the world'
+  version '0.1.1'
+  url 'https://github.com/BladeStorm10/redmine'
   author_url 'Ananomous'
   requires_redmine version_or_higher: '5.1.3'
 end
